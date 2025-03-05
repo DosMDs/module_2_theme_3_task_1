b755c64e085a9d0010c46d32eadc1fc7d9902463
Oleg Skakunov <dosmds@gmail.com>
2025-03-05 18:07:04 +0300

    Доработал стили

--- src/components/information-layout.jsx
+++
@@ -1,9 +0,0 @@
-export const InformationLayout = ({ currentPlayer, isGameEnded, isDraw }) => {
-	return (
-		<>
-			{isDraw && <div>Ничья</div>}
-			{isGameEnded && <div>{`Победа: ${currentPlayer}`}</div>}
-			{!isDraw && !isGameEnded && <div>{`Ходит: ${currentPlayer}`}</div>}
-		</>
-	);
-};
