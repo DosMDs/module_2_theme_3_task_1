cfa1c876dadec51b4881edf9c7848d4d89325edc
Oleg Skakunov <dosmds@gmail.com>
2025-03-12 12:41:50 +0300

    изменил вариант инициализации поля на более правильный

--- src/game.jsx
+++ src/game.jsx
@@ -6,7 +6,7 @@ const Game = () => {
 	const [currentPlayer, setCurrentPlayer] = useState("X");
 	const [isGameEnded, setIsGameEnded] = useState(false);
 	const [isDraw, setIsDraw] = useState(false);
-	const [field, setField] = useState(["", "", "", "", "", "", "", "", ""]);
+	const [field, setField] = useState(new Array(9).fill(""));
 
 	const update = (index) => {
 		if (field[index] !== "" || isGameEnded || isDraw) {
@@ -29,7 +29,7 @@ const Game = () => {
 		setCurrentPlayer("X");
 		setIsGameEnded(false);
 		setIsDraw(false);
-		setField(["", "", "", "", "", "", "", "", ""]);
+		setField(new Array(9).fill(""));
 	};
 
 	return (
